ce218ad39a94d7603fdd75a4e5fcd26f3b7600fd
SAUZET Lény <leny.sauzet@gmail.com>
2025-12-31 06:15:14 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+const signature = localFont({
+  src: './fonts/Signature-December.otf',
+  display: 'swap',
+  variable: '--font-signature',
+});
+
+export const metadata: Metadata = {
+  title: 'The Blog of Leny Sauzet',
+  description:
+    "Hi I'm Lény, and this is my blog. In here, you'll find all the articles I wished I had when I was learning about web development, shaders, real-time 3D on the web, and more.  Each piece I write aims to dive deep into the topics I'm passionate about, while also making complex topics more accessible through interactive playgrounds, visualization, and well detailed walkthroughs. My goal is to give you the tools and intuition to explore further on your own.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${signature.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
